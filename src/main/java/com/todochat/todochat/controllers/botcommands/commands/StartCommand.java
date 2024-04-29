10ea22af49a352299a8aae357892364adb0caa7f
MiltonVera <a00833471@tec.mx>
2024-04-29 17:27:46 -0600

    StartCommand

--- src/main/java/com/todochat/todochat/controllers/botcommands/commands/StartCommand.java
+++ src/main/java/com/todochat/todochat/controllers/botcommands/commands/StartCommand.java
@@ -1,23 +1,21 @@
 package com.todochat.todochat.controllers.botcommands.commands;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 import com.todochat.todochat.controllers.TaskBotController;
 import com.todochat.todochat.controllers.botcommands.BotCommand;
 import com.todochat.todochat.models.AuthToken;
+import com.todochat.todochat.models.Developer;
+import com.todochat.todochat.models.Manager;
 import com.todochat.todochat.services.AuthService;
 import com.todochat.todochat.services.TelegramService;
-import com.todochat.todochat.utils.BotLabels;
- 
+
+
 @Component
 public class StartCommand implements BotCommand {
 
@@ -25,30 +23,71 @@ public class StartCommand implements BotCommand {
     private AuthService authService;
 
     @Override
-    public void executeCommand(Update update, TaskBotController botController,String[] arguments) {
-        
+    public void executeCommand(Update update, TaskBotController botController, String[] arguments) {
+
         // Instanciamos nuestro servicio de telegram
-        TelegramService telegramService = new TelegramService(update,botController);
+        TelegramService telegramService = new TelegramService(update, botController);
 
         // Verificamos si el usuario esta autenticado
         AuthToken auth = authService.authenticate(update);
 
         // Si no esta autenticado le decimos que se tiene que autenticar
         if (auth == null) {
-            telegramService.sendMessage("No te encuentras autenticado, por favor autentícate con el comando /loginDev o /loginManager, por ejemplo /loginDev-correo-contraseña");
+            telegramService.sendMessage(
+                    "No te encuentras autenticado, por favor autentícate con el comando /loginDev o /loginManager, por ejemplo /loginDev-correo-contraseña");
             return;
         }
-
-       
 
         // Si hay autenticacion verificamos si se trata de un desarollador
-        if(auth.getDeveloper() != null){
-            telegramService.addRow(List.of("(VER MIS TAREAS)/myTasks","(AGREGAR TAREA)/addTask"));
-            telegramService.addRow(List.of("(MODIFICAR MIS DATOS)/profile","(CERRAR SESION)/logout"));
-            telegramService.sendMessage("Bienvenido desarrollador "+auth.getDeveloper().getName()+"\n ¿Que deseas hacer?" + "\n Ver tus tareas: /myTasks \n Agregar una tarea: /addTask-nombreTarea-descripcionTarea " );
+        if (auth.getDeveloper() != null) {
+
+            Developer developer = auth.getDeveloper();
+
+            telegramService.addRow(List.of("(VER MIS TAREAS)/myTasks", "(AGREGAR TAREA)/addTask"));
+            telegramService.addRow(List.of("(MODIFICAR MIS DATOS)/profile", "(CERRAR SESION)/logout"));
+            String message = """
+                    Bienvenido desarrollador %s
+                    Tus datos:
+                    Nombre completo $s
+                    Correo: %s
+                    Telefono: %s
+                    Rol: %s
+
+                    ¿Que deseas hacer?
+                    Ver tus tareas: /myTasks
+                    Agregar una tarea: /addTask-nombreTarea-descripcionTarea
+                    """.formatted(developer.getName(), developer.getName() + " " + developer.getLastname(),
+                    developer.getMail(), developer.getPhone(), developer.getRole());
+            telegramService.sendMessage(message);
             return;
         }
 
+        // Si se trata de un manager
+        if (auth.getManager() != null) {
+
+            Manager manager = auth.getManager();
+
+            telegramService.addRow(List.of("(CREAR PROYECTO)/createProject", "(VER MI PROYECTO)/myProject"));
+            telegramService
+                    .addRow(List.of("(VER DESARROLLADORES)/listDevelopers", "(VER TAREAS DE PROYECTO)/projectTasks"));
+            telegramService.addRow("(IR A INICIO)/start");
+
+            String message = """
+                    Bienvenido manager %s
+                    Tus datos:
+                    Nombre completo $s
+                    Correo: %s
+                    Telefono: %s
+                    Rol: %s
+
+                    ¿Que deseas hacer?
+                    Ver tus tareas: /myTasks
+                    Agregar una tarea: /addTask-nombreTarea-descripcionTarea
+                    """.formatted(manager.getName(), manager.getName() + " " + manager.getLastname(), manager.getMail(),
+                    manager.getPhone(), manager.getRole());
+            telegramService.sendMessage(message);
+            return;
+        }
 
     }
 }
